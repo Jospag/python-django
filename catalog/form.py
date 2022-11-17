e78190f2be47ee7de520d8aab295424c2500800c
Emmanuel <jospag4christ@yahoo.com>
2022-11-17 16:52:32 +0100

    update

--- catalog/form.py
+++ catalog/form.py
@@ -12,4 +12,7 @@ class RenewBookForm(forms.Form):
 
         if data < datetime.date.today():
             raise ValidationError(_('Invalid date renewal in past'))
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
+        return data
 
